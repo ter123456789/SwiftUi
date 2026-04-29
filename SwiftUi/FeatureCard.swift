56ae0661208099eaf10d4cc983d2d53132cec74a
thongpop.c <thongpop.c@takeitsolution.com>
2026-04-29 10:20:36 +0700

    first commit

---
+++ SwiftUi/FeatureCard.swift
@@ -0,0 +1,8 @@
+//
+//  FeatureCard.swift
+//  SwiftUi
+//
+//  Created by Thongpop choojit on 28/4/2569 BE.
+//
+
+import Foundation
---
+++ SwiftUi/LandMark.swift
@@ -0,0 +1,8 @@
+//
+//  LandMark.swift
+//  SwiftUi
+//
+//  Created by Thongpop choojit on 28/4/2569 BE.
+//
+
+import Foundation
